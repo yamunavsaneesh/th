aff4b415ce94c2cc4f448d91f52f3fd5ff9ad434
Yamuna V <mail2yamunav@gmail.com>
2020-01-19 12:20:43 +0400

    Update thl.sql

--- thl.sql
+++ thl.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Community v12.09 (32 bit)
-MySQL - 5.1.41 : Database - treasurehunt
+MySQL - 5.1.41 : Database - th
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.1.41 : Database - treasurehunt
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`treasurehunt` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`th` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `treasurehunt`;
+USE `th`;
 
 /*Table structure for table `admin` */
 
